b643a831586340eff5390c7a593ac3f73c5576fe
DaFluffyPotato <cmlsc7@gmail.com>
2021-01-24 00:45:38 -0500

    added cr recalculations to daily tasks

--- regular_tasks.py
+++ regular_tasks.py
@@ -13,5 +13,7 @@ while True:
         if len(list(database.get_actions())) == 0:
             user_ids = [i for i in range(database.get_counter('user_id')['count'])]
             create_action.update_users(user_ids)
+    if timer % (DAY) == 0:
+        create_action.recalculate_cr(database.get_pool_ids(False))
     time.sleep(1)
     timer += 1
