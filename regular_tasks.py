e650678cd64a962431ca079ddbe5fce268a93424
DaFluffyPotato <cmlsc7@gmail.com>
2021-01-25 00:14:16 -0500

    removed cr mixing from daily tasks due to bbbear chaos

--- regular_tasks.py
+++ regular_tasks.py
@@ -14,6 +14,7 @@ while True:
             user_ids = [i for i in range(database.get_counter('user_id')['count'])]
             create_action.update_users(user_ids)
     if timer % (DAY) == 0:
-        create_action.recalculate_cr(database.get_pool_ids(False))
+        for pool_id in database.get_pool_ids(False):
+            create_action.recalculate_cr([pool_id])
     time.sleep(1)
     timer += 1
