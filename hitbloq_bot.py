444be40ec0f0d28124ca7499f8bc3b62a82ee938
DaFluffyPotato <cmlsc7@gmail.com>
2021-02-02 22:25:20 -0500

    added view checking command

--- hitbloq_bot.py
+++ hitbloq_bot.py
@@ -57,6 +57,8 @@ async def on_message(message):
             scoresaber_id = message_args[1]
             create_action.create_user(scoresaber_id)
             await message.channel.send(message.author.mention + ' user ' + scoresaber_id + ' has been added to the action queue.\nhttps://hitbloq.com/actions')
+        if message_args[0] == '!views':
+            await message.channel.send(message.author.mention + ' Hitbloq has accumulated ' + database.get_counter('views')['count'] + ' views!')
     if message.channel.name == ADMIN_COMMANDS_CHANNEL:
         if message_args[0] == '!recalculate_cr':
             map_pools = message_args[1].split(',')
