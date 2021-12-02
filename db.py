d10a8c4b72d41fa59d27487c7c5e63820eb53571
DaFluffyPotato <cmlsc7@gmail.com>
2021-12-02 17:42:16 -0500

    removed last use of ladders collection

--- db.py
+++ db.py
@@ -508,12 +508,12 @@ class HitbloqMongo():
         self.db['ranked_lists'].update_one({'_id': pool_id}, {'$set': {'cr_curve': curve_data}})
 
     def update_rank_histories(self, pool_id, action_id=None):
-        ranked_ladder = self.db['ladders'].find_one({'_id': pool_id})['ladder']
+        ranked_ladder = [user['_id'] for user in self.db['users'].find({}, {'_id': 1}).sort('total_cr.' + pool_id, -1)]
         # this will need to be bundled into one request somehow later on
         for rank, user in enumerate(ranked_ladder):
             self.db['users'].update_one({'_id': user['user']}, {'$push': {'rank_history.' + pool_id: {'$each': [rank + 1], '$slice': -60}}, '$min': {'max_rank.' + pool_id: rank + 1}})
             if action_id:
-                if rank % 20 == 0:
+                if rank % 200 == 0:
                     self.set_action_progress(action_id, rank / len(ranked_ladder))
 
 print('MongoDB requires a password!')
