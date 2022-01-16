74adedab566059bcabcbfda4815838055e9068d1
DaFluffyPotato <cmlsc7@gmail.com>
2022-01-16 12:42:54 -0500

    added funni catch

--- main.py
+++ main.py
@@ -143,6 +143,10 @@ def user_basic_api(user_id):
 def player_rank_api(pool_id, user):
     api_endpoint()
     user = int(user)
+    
+    if pool_id == 'None':
+        return 'stop lol', 500
+
     return api.player_rank_api(pool_id, user)
 
 @app.route('/api/player_ranks', methods=['POST'])
