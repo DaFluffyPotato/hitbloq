be62a70daa6dc60e98f409ee7404903303718df3
DaFluffyPotato <cmlsc7@gmail.com>
2025-06-07 14:55:20 -0400

    added python interpreter selection to actions watcher

--- actions_forever.py
+++ actions_forever.py
@@ -2,7 +2,9 @@ import time
 
 import os
 
+PYTHON_CMD = 'python3.8'
+
 while True:
-    os.system('python3 actions.py')
+    os.system(f'{PYTHON_CMD} actions.py')
     time.sleep(1)
     print('actions rebooted')
