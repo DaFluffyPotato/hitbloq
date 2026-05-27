c73658288d8fd730d62ceacd5bf04f8f261bd63f
DaFluffyPotato <cmlsc7@gmail.com>
2026-05-27 01:00:35 -0400

    hmd fix

--- scoresaber.py
+++ scoresaber.py
@@ -38,33 +38,27 @@ class ScoresaberInterface():
     def convert_score_format(self, scores):
         modified_scores = []
         for score in scores:
-            try:
-                hmd = 'unknown'
-                if ('device' in score['score']) and ('hmd' in score['score']['device']):
-                    hmd = score['score']['device']['hmd']
-                new_score = {
-                    'score': {
-                        'modifiedScore': score['score']['modifiedScore'],
-                        'maxCombo': score['score']['maxCombo'],
-                        'missedNotes': score['score']['missedNotes'],
-                        'badCuts': score['score']['badCuts'],
-                        'hmd': hmd,
-                        'epochTime': score['score']['createdAt'],
-                        'modifiers': ','.join(score['score']['mods']),
-                    },
-                    'leaderboard': {
-                        'songHash': score['leaderboard']['map']['hash'].upper(),
-                        'difficulty': {
-                            'difficultyRaw': score['leaderboard']['difficulty']['rawDifficulty'],
-                        }
-                    },
-                    'src': 'ss',
-                }
-            except Exception as e:
-                f = open('idek.json', 'w')
-                json.dump(score, f)
-                f.close()
-                raise AttributeError(score)
+            hmd = 'unknown'
+            if ('device' in score['score']) and (score['score']['device'] != None) and ('hmd' in score['score']['device']):
+                hmd = score['score']['device']['hmd']
+            new_score = {
+                'score': {
+                    'modifiedScore': score['score']['modifiedScore'],
+                    'maxCombo': score['score']['maxCombo'],
+                    'missedNotes': score['score']['missedNotes'],
+                    'badCuts': score['score']['badCuts'],
+                    'hmd': hmd,
+                    'epochTime': score['score']['createdAt'],
+                    'modifiers': ','.join(score['score']['mods']),
+                },
+                'leaderboard': {
+                    'songHash': score['leaderboard']['map']['hash'].upper(),
+                    'difficulty': {
+                        'difficultyRaw': score['leaderboard']['difficulty']['rawDifficulty'],
+                    }
+                },
+                'src': 'ss',
+            }
             modified_scores.append(new_score)
         return modified_scores
 
